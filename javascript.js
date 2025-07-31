243706a0dab879b9b0b653ef28526a210ea56713
Patrik Demjan <155092026+patodemjan@users.noreply.github.com>
2025-07-31 22:06:40 +0700

    Add files via upload
    uploading new css file for pics and js for pics

--- javascript.js
+++ javascript.js
@@ -10,3 +10,23 @@ fetch("footer.html")
     document.getElementById("footer").innerHTML = data;
   });
 
+
+document.querySelectorAll('.fullscreen-img').forEach(img => {
+    img.addEventListener('click', () => {
+      // Vytvor fullscreen overlay
+      const overlay = document.createElement('div');
+      overlay.classList.add('fullscreen-overlay');
+
+      const fullImg = document.createElement('img');
+      fullImg.src = img.src;
+      fullImg.alt = img.alt;
+
+      overlay.appendChild(fullImg);
+      document.body.appendChild(overlay);
+
+      // Po kliknutí sa zavrie
+      overlay.addEventListener('click', () => {
+        overlay.remove();
+      });
+    });
+  });
